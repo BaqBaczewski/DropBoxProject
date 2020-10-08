8665c2af2ef0065513203457e9f0755ceb819f12
Philip Nagler-Frank <philip.frank@awacademy.de>
2020-10-08 10:38:23 +0200

    spring initializr

---
+++ src/main/java/example/audiohive/app/AudiohiveApplication.java
@@ -0,0 +1,13 @@
+package example.audiohive.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiohiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiohiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/audiohive/app/AudiohiveApplicationTests.java
@@ -0,0 +1,13 @@
+package example.audiohive.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiohiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
