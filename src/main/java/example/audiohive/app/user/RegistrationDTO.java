87b2cfd6cb30ed5962a58cfd7c13f117d4d4d95d
MLysiak2 <Maciej.Lysiak@t-mobile.pl>
2021-01-15 12:46:08 +0100

    Added age check out and tested on locla host

--- src/main/java/example/audiohive/app/user/RegistrationDTO.java
+++ src/main/java/example/audiohive/app/user/RegistrationDTO.java
@@ -1,20 +1,27 @@
 package example.audiohive.app.user;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.time.LocalDate;
 
 public class RegistrationDTO {
     @NotBlank
     @Size(min = 3, max = 50, message = "Minimum length is 3 characters.")
     private final String name;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private final LocalDate date;
+
     @NotBlank
     @Size(min = 5, message = "Minimum length is 5 characters.")
     private final String password1;
     private final String password2;
 
-    public RegistrationDTO(String name, String password1, String password2) {
+    public RegistrationDTO(String name, LocalDate date, String password1, String password2) {
         this.name = name;
+        this.date = date;
         this.password1 = password1;
         this.password2 = password2;
     }
@@ -22,6 +29,8 @@ public class RegistrationDTO {
     public String getName() {
         return name;
     }
+
+    public LocalDate getDate() {return date;}
 
     public String getPassword1() {
         return password1;
