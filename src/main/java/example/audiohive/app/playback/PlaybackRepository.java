3642e56bc3f871e98c67890e0caaa8148f851556
MLysiak2 <Maciej.Lysiak@t-mobile.pl>
2021-01-19 16:06:13 +0100

    Change playback services. Only User playback count

--- src/main/java/example/audiohive/app/playback/PlaybackRepository.java
+++ src/main/java/example/audiohive/app/playback/PlaybackRepository.java
@@ -1,8 +1,12 @@
 package example.audiohive.app.playback;
 
 import example.audiohive.app.sound.Sound;
+import example.audiohive.app.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
 
 public interface PlaybackRepository extends JpaRepository<Playback, String> {
     long countBySound(Sound sound);
+    boolean existsBySoundAndUserAndPlayedAtAfter(Sound sound, User user, Instant instant);
 }
